40dc9373abba3e7b0310fd4df0ef57b71a4f0b23
RafaDom10 <r-domingues@outlook.com>
2021-09-25 22:09:09 -0300

    modal stylization done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import Modal from'react-modal';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
-
+import Modal from 'react-modal';
 import { GlobalStyle } from './styles/global';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 Modal.setAppElement('#root');
 
@@ -22,14 +22,10 @@ export function App() {
     <>
       <GlobalStyle />
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
-
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
-
+      />
       <Dashboard />
     </>
   );
